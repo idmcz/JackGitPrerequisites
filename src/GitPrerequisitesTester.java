afc3587d89126755df22a8503980ebf8446a3d08
Jack Austen <jausten204@gmail.com>
2022-09-14 13:50:13 -0700

    Added gitignore
    Just added the gitignore file

--- src/GitPrerequisitesTester.java
+++ src/GitPrerequisitesTester.java
@@ -2,8 +2,8 @@ import java.io.IOException;
 public class GitPrerequisitesTester{
 
 	public static void main(String[] args) throws IOException{
-		//Blob bob = new Blob ("Input.txt");//makes the blob
-		//System.out.print(bob.getHash());//returns a hash
+		Blob bob = new Blob ("Input.txt");//makes the blob
+		System.out.print(bob.getHash());//returns a hash
 		
 		Index i = new Index ();
 		i.initialize();
@@ -11,7 +11,7 @@ public class GitPrerequisitesTester{
 		i.addBlob("chucky.txt");
 		i.addBlob("Chuckilicious.txt");
 		i.removeBlob("Input.txt");
-		i.removeBlob("Chuckilicious.txt");
+		i.removeBlob("Chuckilicious.txt");//removes chuckilicious
 	}
 
 }
